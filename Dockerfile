a3799e56b7808fcfa15955f49463451911c06fe3
khoa <khoa1999@github.com>
2024-05-23 10:27:21 +0700

    hello

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Dockerfile for React
+# Use the official Node.js image from the Docker Hub
+FROM node:20-alpine as build
+WORKDIR /app
+COPY package*.json /app
+RUN npm install
+COPY . .
+# Copy .env.production file for production build
+COPY .env.production .env
+# Build the application
+CMD ["npm", "run", "build"]
+
+# Copy the custom Nginx configuration file
+#COPY config/nginx/nginx.conf /etc/nginx/conf.d/default.conf
+# Stage 2: Serve the app with Nginx
+#FROM nginx:alpine
+
+# Copy the build files from the previous stage
+#COPY --from=build /app/build /usr/share/nginx/html
+
+# Expose port 80
+#EXPOSE 80
+
+# Start Nginx
+#CMD ["nginx", "-g", "daemon off;"]
--- src/components/login_page/login_page.tsx
+++ src/components/login_page/login_page.tsx
@@ -3,6 +3,7 @@ import styles from './login_page.module.scss';
 import { useState, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import backgroundImage from '../../assets/6106991.jpg'
+import { useNavigate } from "react-router-dom";
 
 export interface LoginProps {
     className?: string;
@@ -11,9 +12,15 @@ export const Login = ({ className }: LoginProps) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [isLoginView, setIsLoginView] = useState(true);
+    const [isMouseHover,setIsMouseHover] = useState(false);
     const [token, setToken, removeToken] = useCookies(['Cookies']);
+    const handleMouseIn = ()=>{setIsMouseHover(true)};
+    const handleMouseOut = ()=>{setIsMouseHover(false)};
+
     const isDisabled = username.length === 0 || password.length === 0;
-    useEffect(() => {}, [token]);
+    useEffect(() => {
+        //
+;    }, [token]);
     const loginClicked = () => {};
     const registerClicked = () => {};
     return (
@@ -25,7 +32,7 @@ export const Login = ({ className }: LoginProps) => {
             <div className="login-container">
                 <label htmlFor="username">Username</label>
                 <br />
-                <input
+                <input className={classNames(styles.LoginInput, className)}
                     id="username"
                     type="text"
                     placeholder="Username"
@@ -35,7 +42,7 @@ export const Login = ({ className }: LoginProps) => {
                 <br />
                 <label htmlFor="password">Password</label>
                 <br />
-                <input
+                <input className={classNames(styles.LoginInput, className)}
                     id="password"
                     type="password"
                     placeholder="Password"
@@ -55,11 +62,15 @@ export const Login = ({ className }: LoginProps) => {
                     </button>
                 )}
                 {isLoginView ? (
-                    <p onClick={() => setIsLoginView(false)}>
+                    <p style={{ color: isMouseHover ? 'blue' : 'black' }}
+                    onClick={() => setIsLoginView(false)} 
+                    onMouseOver={handleMouseIn} onMouseOut = {handleMouseOut}>
                         You don't have an account? Register here!
                     </p>
                 ) : (
-                    <p onClick={() => setIsLoginView(true)}>
+                    <p style={{ color: isMouseHover ? 'blue' : 'black' }}
+                    onClick={() => setIsLoginView(true)}
+                    onMouseOver={handleMouseIn} onMouseOut = {handleMouseOut}>
                         You already have an account? Login here!
                     </p>
                 )}
